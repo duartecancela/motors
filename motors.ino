4f4aec680d02866f856c0b5099b6016203868dd6
Duarte Cancela <duartecancela@gmail.com>
2021-08-22 17:37:00 +0100

    add functions

--- motors.ino
+++ motors.ino
@@ -1,65 +1,102 @@
-/* 
-motors.ino
-Duarte Cancela
-15/08/2021 
-----
-DFC-77 robot motors control code
+/*
+  motors.ino
+  Duarte Cancela
+  15/08/2021
+  ----
+  DFC-77 robot motors control code
 */
- 
-// initialize pins
+
+// initialize pins for the L298N motors driver
 // Right motor
-int rightMotorEn = 9;
-int rightMotorIn1 = 4;
-int rightMotorIn2 = 5;
+int rightMotorsEn = 9;
+int rightMotorsIn1 = 4;
+int rightMotorsIn2 = 5;
 // left motor
-int leftMotorEn = 10;
-int leftMotorIn3 = 6;
-int leftMotorIn4 = 7;
-  
+int leftMotorsEn = 10;
+int leftMotorsIn3 = 6;
+int leftMotorsIn4 = 7;
+
+int motorsSpeedLeft = 0;
+int motorsSpeedRight = 0;
+
 void setup()
 {
- // Define output pins
- pinMode(rightMotorIn1, OUTPUT);
- pinMode(rightMotorIn2, OUTPUT);
- pinMode(leftMotorIn3, OUTPUT);
- pinMode(leftMotorIn4, OUTPUT);
-}
-  
-void loop()
-{
- // Rotate right motor clockwise
- analogWrite(rightMotorEn, 125);
- digitalWrite(rightMotorIn1, HIGH);
- digitalWrite(rightMotorIn2, LOW);
- delay(4000);
- // Stop right motor
- digitalWrite(rightMotorIn1, HIGH);
- digitalWrite(rightMotorIn2, HIGH);
- delay(500);
- // Rotate  left motor clockwise
- digitalWrite(leftMotorIn3, HIGH);
- digitalWrite(leftMotorIn4, LOW);
- delay(4000);
- // Stop left motor
- digitalWrite(leftMotorIn3, HIGH);
- digitalWrite(leftMotorIn4, HIGH);
- delay(500);
- 
- // Rotate right motor counterclockwise
- digitalWrite(rightMotorIn1, LOW);
- digitalWrite(rightMotorIn2, HIGH);
- delay(4000);
- // Stop right motor
- digitalWrite(rightMotorIn1, HIGH);
- digitalWrite(rightMotorIn2, HIGH);
- delay(500);
- //Rotate left motor counterclockwise
- digitalWrite(leftMotorIn3, LOW);
- digitalWrite(leftMotorIn4, HIGH);
- delay(4000);
- // Stop left motor
- digitalWrite(leftMotorIn3, HIGH);
- digitalWrite(leftMotorIn4, HIGH);
- delay(500);
+  Serial.begin(9600);
+  // Define output pins
+  pinMode(rightMotorsIn1, OUTPUT);
+  pinMode(rightMotorsIn2, OUTPUT);
+  pinMode(leftMotorsIn3, OUTPUT);
+  pinMode(leftMotorsIn4, OUTPUT);
 }
 
+// Rotate right motor forward
+void rotateRightMotorsFWD(){
+  Serial.println("Rotate right motors forward");
+  analogWrite(rightMotorsEn, 50); // speed
+  digitalWrite(rightMotorsIn1, HIGH);
+  digitalWrite(rightMotorsIn2, LOW);
+  delay(4000);
+}
+
+// Rotate  left motor forward
+void rotateLeftMotorsFWD(){
+  Serial.println("Rotate  left motors forward");
+  analogWrite(leftMotorsEn, 50); // speed
+  digitalWrite(leftMotorsIn3, HIGH);
+  digitalWrite(leftMotorsIn4, LOW);
+  delay(4000);
+}
+
+// Rotate right motors backward
+void rotateRightMotorsBWD(){
+  Serial.println("Rotate right motors backward");
+  digitalWrite(rightMotorsIn1, LOW);
+  digitalWrite(rightMotorsIn2, HIGH);
+  delay(4000);
+}
+
+//Rotate left motor backward
+void rotateLeftMotorsBWD(){
+  Serial.println("Rotate left motors backward");
+  digitalWrite(leftMotorsIn3, LOW);
+  digitalWrite(leftMotorsIn4, HIGH);
+  delay(4000);
+}
+
+// Stop right motors
+void stopRightMotors(){
+  Serial.println("Stop right motors");
+  digitalWrite(rightMotorsIn1, HIGH);
+  digitalWrite(rightMotorsIn2, HIGH);
+  delay(500);
+}
+
+// Stop left motors
+void stopLeftMotors(){
+  Serial.println("Stop left motors");
+  digitalWrite(leftMotorsIn3, HIGH);
+  digitalWrite(leftMotorsIn4, HIGH);
+  delay(500);
+}
+
+void loop()
+{
+  int xAxis = analogRead(A0); // Read Joysticks X-axis
+  int yAxis = analogRead(A1); // Read Joysticks Y-axis
+
+  rotateRightMotorsFWD();
+  
+  stopRightMotors();
+
+  rotateLeftMotorsFWD();
+  
+  stopLeftMotors();
+
+  rotateRightMotorsBWD();
+  
+  stopRightMotors();
+  
+  rotateLeftMotorsBWD();
+  
+  stopLeftMotors();
+}
